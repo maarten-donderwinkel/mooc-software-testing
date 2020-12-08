9cd3659da528570a4df5f787486a8771372e2abe
Maarten Donderwinkel <maartend@topdesk.com>
2020-12-08 19:23:10 +0100

    Complete the test class for discountapplier

--- src/test/java/tudelft/discount/DiscountApplierTest.java
+++ src/test/java/tudelft/discount/DiscountApplierTest.java
@@ -1,5 +1,30 @@
 package tudelft.discount;
 
-public class DiscountApplierTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import tudelft.invoicemocked.Invoice;
+import tudelft.invoicemocked.InvoiceDao;
+import tudelft.invoicemocked.InvoiceFilter;
 
+import java.util.Arrays;
+import java.util.List;
+
+public class DiscountApplierTest {
+    @Test
+    void DiscountApplier() {
+
+        Product home = new Product("Google Home Mini",100,"HOME");
+        Product business = new Product("Google Home Business",100,"BUSINESS");
+
+        ProductDao dao = Mockito.mock(ProductDao.class);
+        List<Product> results = Arrays.asList(home,business);
+        Mockito.when(dao.all()).thenReturn(results);
+
+        DiscountApplier appliedDiscount = new DiscountApplier(dao);
+        appliedDiscount.setNewPrices();
+
+        Assertions.assertEquals(90,home.getPrice());
+        Assertions.assertEquals(110,Math.round(business.getPrice()));
+    }
 }
